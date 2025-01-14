e81673346f3f861dcf1c4df2277f18b7f16f0761
Aomariayman <aymanaomaripro@gmail.Com>
2025-01-14 22:12:44 +0100

    for hassan

--- src/main.js
+++ src/main.js
@@ -5,15 +5,10 @@ import router from "./router";
 import globalComponents from "./global-components";
 import utils from "./utils";
 import "./assets/css/app.css";
-import VueLazyload from "vue-lazyload";
 
 const app = createApp(App).use(router).use(createPinia());
 
 globalComponents(app);
 utils(app);
-app.use(VueLazyload, {
-  loading: new URL("./assets/loading.gif", import.meta.url).href, // Placeholder while loading
-  error: new URL("./assets/error.png", import.meta.url).href, // Placeholder on error
-});
 
 app.mount("#app");
